ada5d5480015c93a1cd1364a831957c41074937d
shekurika <shekurika@gmail.com>
2021-09-19 03:36:14 +0200

    release

---
+++ DailySpecialOrders/ModConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailySpecialOrders
+{
+    public class ModConfig
+    {
+        public bool RefreshAfterPicking { get; set; } = true;
+        public bool RefreshTuesday { get; set; } = true;
+        public bool RefreshWednesday { get; set; } = true;
+        public bool RefreshThursday { get; set; } = true;
+        public bool RefreshFriday { get; set; } = true;
+        public bool RefreshSaturday { get; set; } = true;
+        public bool RefreshSunday { get; set; } = true;
+    }
+}
